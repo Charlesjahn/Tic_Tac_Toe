afc58b2e3c398f5b53f54b095c8ae47b99de46a7
Charles Franklin Jahn <charles.jahn@gmail.com>
2022-10-12 16:23:06 +0100

    4
    applying choose to start the game

--- app/src/main/java/com/cfjahnprojects/tic_tac_toe/MainActivity.java
+++ app/src/main/java/com/cfjahnprojects/tic_tac_toe/MainActivity.java
@@ -8,7 +8,10 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     private int turn =0;
-    private int[][] board;
+    private int[][] board = {
+            {-1,-1,-1,},
+            {-1,-1,-1,},
+            {-1,-1,-1,}};
     private int player = 0;
     private int[][] imagePositions = {
             {R.id.place00,R.id.place01,R.id.place02,},
@@ -26,22 +29,38 @@ public class MainActivity extends AppCompatActivity {
 
         if((this.turn%2) == 0){
             imageView.setImageResource(R.drawable.xttt);
-            setBoardGame(idImage);
+            setBoardGame(idImage,this.turn%2);
         }else{
             imageView.setImageResource(R.drawable.circlettt);
-            setBoardGame(idImage);
+            setBoardGame(idImage, this.turn%2);
         }
+
+//        printTable();
         this.turn +=1;
         imageView.setOnClickListener(null);
     }
+    public void xStart(View view){
 
-    private void setBoardGame(int idPlace){
-        for(int i=0;i>3;i++){
-            for(int j=0;j>3;j++){
+    }
+    public void circleStart(View view){
+
+    }
+
+    private void setBoardGame(int idPlace, int x){
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
                 if(this.imagePositions[i][j]==idPlace){
-                    this.board[i][j]=this.turn%2;
+                    this.board[i][j]=x;
                 }
             }
+        }
+    }
+    private void printTable(){
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                System.out.print(this.board[i][j]+" ");
+            }
+            System.out.println("\n");
         }
     }
 

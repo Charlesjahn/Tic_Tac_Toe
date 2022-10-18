e4e2da603e9cea2d8edf17bf5119584924d2ac7d
Charles Franklin Jahn <charles.jahn@gmail.com>
2022-10-18 13:28:21 +0100

    Win /loss

--- app/src/main/java/com/cfjahnprojects/tic_tac_toe/MainActivity.java
+++ app/src/main/java/com/cfjahnprojects/tic_tac_toe/MainActivity.java
@@ -76,4 +76,38 @@ public class MainActivity extends AppCompatActivity {
         ImageView imageViewX = findViewById(R.id.xStart);
         imageViewX.setOnClickListener(null);
     }
+    public boolean isWon(){
+        //return true if someone has won the game
+
+        for(int row=0; row<board.length; row++){
+
+            //check all the rows
+            if (((board[row][0] == board[row][1]) && (board[row][0]==board[row][2])) && (board[row][0] != -1)) {
+
+                return true;
+            }
+        }
+
+        for(int col=0; col<board.length; col++){
+            //check all the columns
+            if (((board[0][col] == board[1][col]) && (board[0][col]==board[2][col])) && (board[0][col] != -1)) {
+
+                return true;
+            }
+        }
+
+        if( (board[0][0] == board[1][1]) && (board[0][0] == board[2][2]) && (board[0][0] != -1)){
+            //diagonal winner 1
+            return true;
+        }
+        else if ((board[0][2] == board[1][1]) && (board[0][2] == board[2][0]) && (board[0][2] != -1)){
+            //diagonal winner 2
+            return true;
+        }
+        else{
+            //no winner
+            return false;
+        }
+
+    }
 }
